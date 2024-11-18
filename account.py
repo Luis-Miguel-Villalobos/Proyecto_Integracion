3f8499d4f0ca255ae61705d53bee32b02fd3eb27
Luis Miguel <92600110+Luis-Miguel-Villalobos@users.noreply.github.com>
2024-11-17 22:21:22 -0600

    Add files via upload

--- account.py
+++ account.py
@@ -1,5 +1,3 @@
-API_Key = "d1kdQx7sNvHhpJkLQhlrl6KG6"
-API_Key_Secret = "GeUCia8jfRhxUOqh4CrrchVmq7EzXeKRHZsZEX4CpaxgNeyugH"
-Access_Token= "1839371005137203200-nCIHzGIc42KimttDrxy3fNPDDap5rH"
-Access_Token_Secret = "0WEy61yMq3XcjcQ0yt35jzG1B1V6naP08dXg51KXk3F2v"
-#print("hola")
+USERNAME = '@Villalobos35839'
+EMAIL = 'lsmglvillalobos19@gmail.com'
+PASSWORD = '&Diminombre19%'
--- index.py
+++ index.py
@@ -1,11 +1,45 @@
-import tweepy
-import account
+import asyncio, csv, json, account as a
+from twikit import Client
 
-client= tweepy.OAuthHandler(account.API_Key, account.API_Key_Secret)
-client.set_access_token(account.Access_Token, account.Access_Token_Secret)
-api = tweepy.API(client)
+# Initialize client
+client = Client('en-US')
 
-tweet= "hola este es mi primer tweet con Tweepy"
-response= api.update_status(status=tweet)
+async def main():
+    await client.login(
+        auth_info_1=a.USERNAME ,
+        auth_info_2=a.EMAIL,
+        password=a.PASSWORD
+    )
+    print("Inicie sesion en X")
 
-print("Tweet posteado bien",response)
+    try:
+        tweets = await client.search_tweet('from:SATMX', 'top')
+
+        # Abre el archivo CSV en modo escritura
+        with open('tweets.csv', mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+
+            # Escribe la cabecera del CSV solo si el archivo esta vacio+
+            if file.tell() == 0:
+                writer.writerow(['user_name', 'tweet_text', 'created_at'])
+
+            for tweet in tweets:
+                # Convierte el tweet a formato JSON
+                tweet_json = json.dumps({
+                    'user_name': tweet.user.name,
+                    'tweet_text': tweet.text,
+                    'created_at': tweet.created_at
+                })
+                # Escribe el tweet en el archivo CSV
+                writer.writerow([tweet.user.name, tweet.text, tweet.created_at])
+                #await client.logout()
+
+    except Exception as e:
+        print(f"No pude recolectar tweets: {e}")
+    else:
+        print("Tweets recolectados con exito")
+
+try:
+    asyncio.run(main())
+except Exception as e:
+    print(f"No pude iniciar sesion, Error: {e}")
